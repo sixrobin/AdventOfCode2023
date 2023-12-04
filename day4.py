025b5159268fb09abb7502517abf515cefcca055
Robin Six <six.robin@hotmail.fr>
2023-12-04 12:51:00 +0100

    Day 4

---
+++ day4.py
@@ -0,0 +1,19 @@
+if __name__ == '__main__':
+    with open('input_day4.txt') as file:
+        lines = file.readlines()
+        points_sum = 0
+        cards_counts = [1] * len(lines)
+
+        for i in range(len(lines)):
+            numbers_split = lines[i].split(':')[1].split('|')
+            winning_numbers_count = sum(map(lambda x: x in numbers_split[0].split(), numbers_split[1].split()))
+
+            if winning_numbers_count > 0:
+                points_sum += pow(2, winning_numbers_count - 1)
+                for j in range(i + 1, i + winning_numbers_count + 1):
+                    cards_counts[j] += cards_counts[i]
+
+        cards_count = sum(cards_counts)
+
+        print(f'Cards count: {cards_count}')
+        print(f'Points sums: {points_sum}')
