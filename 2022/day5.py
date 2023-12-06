50f06e31ac86923fc541851e601fb6107c093150
Robin Six <55784799+sixrobin@users.noreply.github.com>
2023-12-06 12:11:13 +0100

    2022 Day 5

--- 2022/day5.py
+++ 2022/day5.py
@@ -1,3 +1,5 @@
+import re
+
 if __name__ == '__main__':
     with open('input_day5.txt') as file:
         lines = file.read().splitlines()
@@ -10,13 +12,28 @@ if __name__ == '__main__':
                 stacks = [[] for x in range(int(lines[i].strip()[-1]))]
                 break
 
+        # Fill stacks.
         for i in range(stack_indices_line - 1, -1, -1):
-            line_fill = lines[i].replace(' [', '[').replace('] ', ']').replace('   ', '[.]')
+            line_fill = lines[i].replace(' [', '[')
+            line_fill = line_fill.replace('] ', ']')
+            line_fill = line_fill.replace('   ', '[.]')
+            line_fill = re.sub(' ', '', line_fill)
             for x in range(line_fill.count('[')):
                 crate = line_fill[1 + x * 3]
                 if crate != '.':
                     stacks[x].append(crate)
 
+        # Process move instructions.
         for i in range(stack_indices_line + 2, len(lines)):
-            # TODO: Process move.
-            print(lines[i])
+            m = [int(i) for i in lines[i].split() if i.isdigit()]
+            buffer = []
+            for x in range(m[0]):
+                crate = stacks[m[1] - 1].pop()
+                buffer.append(crate)
+            while buffer:
+                stacks[m[2] - 1].append(buffer.pop())
+
+        result = ''
+        for s in stacks:
+            result += s[-1]
+        print(f'Result: {result}')
