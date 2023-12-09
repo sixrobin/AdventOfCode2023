56d7ad941adde77d7cd860f118e6216aaafbcf4d
Robin Six <55784799+sixrobin@users.noreply.github.com>
2023-12-09 23:47:26 +0100

    2023 Day 9 part 2

--- 2023/day9.py
+++ 2023/day9.py
@@ -1,7 +1,7 @@
 if __name__ == '__main__':
     with open('input_day9.txt') as file:
         lines = file.read().splitlines()
-        result = 0
+        p1_result, p2_result = 0, 0
 
         for line in lines:
             seqs = [[int(x) for x in line.split()]]
@@ -14,6 +14,10 @@ if __name__ == '__main__':
 
             for i in range(len(seqs) - 1, 0, -1):
                 seqs[i - 1].append(seqs[i][-1] + seqs[i - 1][-1])
-            result += seqs[0][-1]
+                seqs[i - 1].insert(0, seqs[i - 1][0] - seqs[i][0])
 
-        print(result)
+            p1_result += seqs[0][-1]
+            p2_result += seqs[0][0]
+
+        print(f'Part 1: {p1_result}')
+        print(f'Part 2: {p2_result}')
