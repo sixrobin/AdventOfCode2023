cd628d3f654ea0801f4816d09b4c578cfebb42dc
Robin Six <55784799+sixrobin@users.noreply.github.com>
2023-12-11 23:52:15 +0100

    2023 Day 11 part1 and part2 prints

--- 2023/day11.py
+++ 2023/day11.py
@@ -20,18 +20,20 @@ if __name__ == '__main__':
                 if lines[y][x] == '#':
                     galaxies.append((x, y))
 
-        path_lengths_sum = 0
-        for ga in range(len(galaxies)):
-            for gb in range(ga + 1, len(galaxies)):
-                ga_x, ga_y = galaxies[ga]
-                gb_x, gb_y = galaxies[gb]
-                if ga_x > gb_x:
-                    ga_x, gb_x = gb_x, ga_x
-                if ga_y > gb_y:
-                    ga_y, gb_y = gb_y, ga_y
-                for x in range(ga_x, gb_x):
-                    path_lengths_sum += 1 if x not in empty_cols else 1000000
-                for y in range(ga_y, gb_y):
-                    path_lengths_sum += 1 if y not in empty_rows else 1000000
+        for i in range(2):
+            expansion = 2 if i == 0 else 10**6
+            path_lengths_sum = 0
+            for ga in range(len(galaxies)):
+                for gb in range(ga + 1, len(galaxies)):
+                    ga_x, ga_y = galaxies[ga]
+                    gb_x, gb_y = galaxies[gb]
+                    if ga_x > gb_x:
+                        ga_x, gb_x = gb_x, ga_x
+                    if ga_y > gb_y:
+                        ga_y, gb_y = gb_y, ga_y
+                    for x in range(ga_x, gb_x):
+                        path_lengths_sum += 1 if x not in empty_cols else expansion
+                    for y in range(ga_y, gb_y):
+                        path_lengths_sum += 1 if y not in empty_rows else expansion
 
-        print(f'All pairs shortest path lengths sum: {path_lengths_sum}')
+            print(f'All pairs shortest path lengths sum (expansion of {expansion}): {path_lengths_sum}')
